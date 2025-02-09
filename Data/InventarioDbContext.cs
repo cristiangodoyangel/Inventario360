d5c63d3c03597107a41fe86f9bdcf5813bae1a4b
cristiangodoyangel <creacciones@gmail.com>
2025-02-09 10:35:37 -0300

    🚀 Configuración inicial completa: Conexión a SQL Server, Entity Framework, Identity y vistas básicas

---
+++ Data/InventarioDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Inventario360.Models;
+
+namespace Inventario360.Data
+{
+    public class InventarioDbContext : IdentityDbContext<Usuario>
+    {
+        public InventarioDbContext(DbContextOptions<InventarioDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Inventario360.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public required string NombreCompleto { get; set; }
+    }
+}
