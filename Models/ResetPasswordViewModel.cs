f834c7e3a3d833bb1c785ffe1983d4dcffdd636c
cristiangodoyangel <creacciones@gmail.com>
2025-02-09 21:28:53 -0300

    feat: Se creó Login.cshtml y se diseñó correctamente

---
+++ Models/ResetPasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ResetPasswordViewModel
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Token { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+    public string ConfirmPassword { get; set; }
+}
