81139f097a509208e9b6d02c39f85fe39e1fd85d
cristiangodoyangel <creacciones@gmail.com>
2025-03-03 18:17:07 -0300

    feat: imagen con espacio en tabla de solicitud de materiales

---
+++ wwwroot/js/reportes.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    fetch("/Reportes/ObtenerMetricas")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("totalInventario").textContent = data.totalInventario;
+            document.getElementById("totalSalidas").textContent = data.totalSalidas;
+            document.getElementById("totalSolicitudes").textContent = data.totalSolicitudes;
+        })
+        .catch(error => console.error("Error al cargar métricas:", error));
+});
