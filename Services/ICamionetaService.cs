d6afb2be80920494120a789b8786fef8f69fbcd2
cristiangodoyangel <creacciones@gmail.com>
2025-03-09 15:34:56 -0300

    feat: Solicitud de material funcionando como antes.

---
+++ Services/ICamionetaService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Inventario360.Models;
+
+namespace Inventario360.Services
+{
+    public interface ICamionetaService
+    {
+        Task<IEnumerable<Camioneta>> ObtenerCamionetas();
+        Task<Camioneta> ObtenerCamionetaPorId(int id);
+        Task<bool> CrearCamioneta(Camioneta camioneta);
+        Task<bool> EditarCamioneta(Camioneta camioneta);
+        Task<bool> EliminarCamioneta(int id);
+    }
+}
