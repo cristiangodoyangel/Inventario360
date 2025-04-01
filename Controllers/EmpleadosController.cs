6362f4bb955c63d71b707c08033bea0fa8bfa2d4
cristiangodoyangel <creacciones@gmail.com>
2025-04-01 01:52:56 -0300

    Módulo Empleados funcional: modelo, servicio, controlador, vistas Crear/Index/Eliminar, validación contra salidas de bodega

--- Controllers/EmpleadosController.cs
+++ Controllers/EmpleadosController.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
+using Inventario360.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inventario360.Controllers
 {
@@ -13,12 +14,13 @@ namespace Inventario360.Controllers
     {
         private readonly IEmpleadoService _empleadoService;
         private readonly IFichaEmpleadoService _fichaEmpleadoService;
+        private readonly InventarioDbContext _context;
 
-
-        public EmpleadosController(IEmpleadoService empleadoService, IFichaEmpleadoService fichaEmpleadoService)
+        public EmpleadosController(IEmpleadoService empleadoService, IFichaEmpleadoService fichaEmpleadoService, InventarioDbContext context)
         {
             _empleadoService = empleadoService;
             _fichaEmpleadoService = fichaEmpleadoService;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
@@ -34,26 +36,23 @@ namespace Inventario360.Controllers
             return View(empleado);
         }
 
-        public async Task<IActionResult> Crear()
+        public IActionResult Crear()
         {
-            var empleados = await _empleadoService.ObtenerTodos();
-            ViewBag.Empleados = new SelectList(empleados, "ID", "Nombre");
-            return View();
+            return View(new Empleado());
         }
 
         [HttpPost]
-        public async Task<IActionResult> Crear(FichaEmpleado ficha)
+        public async Task<IActionResult> Crear(Empleado empleado)
         {
             if (!ModelState.IsValid)
             {
-                var empleados = await _empleadoService.ObtenerTodos();
-                ViewBag.Empleados = new SelectList(empleados, "ID", "Nombre");
-                return View(ficha);
+                return View(empleado);
             }
 
-            await _fichaEmpleadoService.Agregar(ficha);
+            await _empleadoService.Agregar(empleado);
             return RedirectToAction(nameof(Index));
         }
+
 
         public async Task<IActionResult> Editar(int id)
         {
@@ -81,7 +80,32 @@ namespace Inventario360.Controllers
         [HttpPost, ActionName("Eliminar")]
         public async Task<IActionResult> ConfirmarEliminar(int id)
         {
-            await _empleadoService.Eliminar(id);
+            try
+            {
+                var empleado = await _empleadoService.ObtenerPorId(id);
+                if (empleado == null)
+                {
+                    TempData["Error"] = "No se encontró el empleado.";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                var tieneSalidas = await _context.SalidaDeBodega
+                    .AnyAsync(s => s.Solicitante == id || s.ResponsableEntrega == id);
+                if (tieneSalidas)
+                {
+                    TempData["Error"] = "No se puede eliminar el empleado porque está asociado a una Salida de Bodega.";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                _context.Empleado.Remove(empleado);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "Empleado eliminado correctamente.";
+            }
+            catch
+            {
+                TempData["Error"] = "Error interno al intentar eliminar el empleado.";
+            }
+
             return RedirectToAction(nameof(Index));
         }
     }
